43d7b42b918e41ba0af02ae7e1abe30178290fdd
Gaurav Shukla <gauravshukla@Gauravs-MacBook.local>
2024-12-23 20:26:25 +0530

    update projects -F

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -7,11 +7,11 @@ function Projects(){
                   <h1 className="display-6 mb-5 mx-2">Projects</h1>
             <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
 
-                <Card source="https://github.com/GodZilla-7/Whatsapp_qr" live="https://whatsappqr.up.railway.app/" title="hello" content="erlkadfnv;lnvoaj;ndvldsnvlknfm" img="../public/projects/project1.png"/>
-                <CardP source="https://github.com/GodZilla-7/Ecom_calculator" live="https://ecom-calculator.vercel.app/" title="hello" content="erlkadfnv;lnvoaj;ndvldsnvlknfm" img="../public/projects/project2.png"/>
-                <Card source="https://github.com/GodZilla-7/calculator" live="https://calculator-gamma-pearl.vercel.app/" title="hello" content="erlkadfnv;lnvoaj;ndvldsnvlknfm" img="../public/projects/project3.png"/>
-                <CardP source="https://github.com/GodZilla-7/shopBraze" live="https://shopbraze.com/" title="hello" content="erlkadfnv;lnvoaj;ndvldsnvlknfm" img="../public/projects/project4.png"/>
-                <Card source="https://github.com/GodZilla-7/cloneYT" live="https://clone-yt-phi.vercel.app/" title="hello" content="erlkadfnv;lnvoaj;ndvldsnvlknfm" img="../public/projects/project5.png"/>
+                <Card source="https://github.com/GodZilla-7/Whatsapp_qr" live="https://whatsappqr.up.railway.app/" title="hello" content="erlkadfnv;lnvoaj;ndvldsnvlknfm" img="./projects/project1.png"/>
+                <CardP source="https://github.com/GodZilla-7/Ecom_calculator" live="https://ecom-calculator.vercel.app/" title="hello" content="erlkadfnv;lnvoaj;ndvldsnvlknfm" img="./projects/project2.png"/>
+                <Card source="https://github.com/GodZilla-7/calculator" live="https://calculator-gamma-pearl.vercel.app/" title="hello" content="erlkadfnv;lnvoaj;ndvldsnvlknfm" img="./projects/project3.png"/>
+                <CardP source="https://github.com/GodZilla-7/shopBraze" live="https://shopbraze.com/" title="hello" content="erlkadfnv;lnvoaj;ndvldsnvlknfm" img="./projects/project4.png"/>
+                <Card source="https://github.com/GodZilla-7/cloneYT" live="https://clone-yt-phi.vercel.app/" title="hello" content="erlkadfnv;lnvoaj;ndvldsnvlknfm" img="./projects/project5.png"/>
 
 
             </div>
