bb92db75e2e013a0f70912facbda17c00d113de8
Gaurav Shukla <140684107+GodZilla-7@users.noreply.github.com>
2024-12-24 10:33:03 +0530

    Update collab.jsx

--- src/components/collab.jsx
+++ src/components/collab.jsx
@@ -7,12 +7,6 @@ function Collab(){
                   <h1 className="display-6 mb-5 mx-2">Contribution</h1>
             <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
                 <Card source="https://github.com/GodZilla-7/Whatsapp_qr" live="https://whatsappqr.up.railway.app/" title="hello" content="erlkadfnv;lnvoaj;ndvldsnvlknfm" img="./projects/project1.png"/>
-                <CardP source="https://github.com/GodZilla-7/Ecom_calculator" live="https://ecom-calculator.vercel.app/" title="hello" content="erlkadfnv;lnvoaj;ndvldsnvlknfm" img="./projects/project2.png"/>
-                <Card source="https://github.com/GodZilla-7/calculator" live="https://calculator-gamma-pearl.vercel.app/" title="hello" content="erlkadfnv;lnvoaj;ndvldsnvlknfm" img="./projects/project3.png"/>
-                <CardP source="https://github.com/GodZilla-7/shopBraze" live="https://shopbraze.com/" title="hello" content="erlkadfnv;lnvoaj;ndvldsnvlknfm" img="./projects/project4.png"/>
-                <Card source="https://github.com/GodZilla-7/cloneYT" live="https://clone-yt-phi.vercel.app/" title="hello" content="erlkadfnv;lnvoaj;ndvldsnvlknfm" img="./projects/project5.png"/>
-
-
             </div>
         </div>
     );
