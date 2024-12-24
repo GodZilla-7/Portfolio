ffe12ad96e48ee769b95178e262d693211f62cde
Gaurav Shukla <140684107+GodZilla-7@users.noreply.github.com>
2024-12-24 10:31:03 +0530

    Update collab.jsx

--- src/components/collab.jsx
+++ src/components/collab.jsx
@@ -4,12 +4,15 @@ import CardP from "./CardP";
 function Collab(){
     return (
         <div className="album py-5 flex flex-col items-center border-bottom">
-                  <h1 className="display-6 mb-5 mx-2">Collaborations</h1>
+                  <h1 className="display-6 mb-5 mx-2">Contribution</h1>
             <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
-            <Card title="hello" content="er" img="./collab_projects/collab1.png"/>
-            </div>
-                        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
-            <Card title="hello" content="er" img="./collab_projects/collab1.png"/>
+                <Card source="https://github.com/GodZilla-7/Whatsapp_qr" live="https://whatsappqr.up.railway.app/" title="hello" content="erlkadfnv;lnvoaj;ndvldsnvlknfm" img="./projects/project1.png"/>
+                <CardP source="https://github.com/GodZilla-7/Ecom_calculator" live="https://ecom-calculator.vercel.app/" title="hello" content="erlkadfnv;lnvoaj;ndvldsnvlknfm" img="./projects/project2.png"/>
+                <Card source="https://github.com/GodZilla-7/calculator" live="https://calculator-gamma-pearl.vercel.app/" title="hello" content="erlkadfnv;lnvoaj;ndvldsnvlknfm" img="./projects/project3.png"/>
+                <CardP source="https://github.com/GodZilla-7/shopBraze" live="https://shopbraze.com/" title="hello" content="erlkadfnv;lnvoaj;ndvldsnvlknfm" img="./projects/project4.png"/>
+                <Card source="https://github.com/GodZilla-7/cloneYT" live="https://clone-yt-phi.vercel.app/" title="hello" content="erlkadfnv;lnvoaj;ndvldsnvlknfm" img="./projects/project5.png"/>
+
+
             </div>
         </div>
     );
