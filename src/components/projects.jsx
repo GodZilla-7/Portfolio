aa7b2fd9f1d75177ba4ba505c69fc7cad7ff2934
Gaurav Shukla <gouravshukla337@gmail.com>
2025-03-13 11:03:31 +0530

    3 commit

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -8,6 +8,7 @@ function Projects(){
             <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
 <Card source="https://github.com/GodZilla-7/ecom" live="https://ecom-taupe-tau.vercel.app/" title="ShopBraze" content="An e-commerce website offering a stylish collection of female wear with a seamless shopping experience" img="./projects/project0.png"/>
             
+                <Card source="https://github.com/GodZilla-7/TaskFlow_backend" live="https://taaskfloww.vercel.app/" title="TaskFloww" content="A minimalist to-do website with customizable live wallpaper for a sleek, distraction-free productivity experience." img="./projects/project7.png"/>
                 <Card source="https://github.com/GodZilla-7/Whatsapp_qr" live="https://whatsappqr.up.railway.app/" title="Whatsapp QR generator" content="Enter Mobile no to get a qr that redirects to Whatsapp chat" img="./projects/project1.png"/>
                 <CardP source="https://github.com/GodZilla-7/Ecom_calculator" live="https://ecom-calculator.vercel.app/" title="E-com Calculator" content="An E-com tool that simplifies Selling price and Break even calculations" img="./projects/project2.png"/>
                 <Card source="https://github.com/GodZilla-7/calculator" live="https://calculator-gamma-pearl.vercel.app/" title="Calculator" content="Made a functioning Calculator for learning purpose" img="./projects/project3.png"/>
